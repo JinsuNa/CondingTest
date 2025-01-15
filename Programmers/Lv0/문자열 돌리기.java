8a101d3009a391f329744670f61a15c6437f9b14
JinsuNa <shs1401009@gmail.com>
2025-01-15 18:03:27 +0900

    Create 문자열 돌리기.java

---
+++ Programmers/Lv0/문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(char s : a.toCharArray()){
+            System.out.println(s);
+        }
+    }
+}
+
+// 앞에 했던 for 문을 사용하면 쉽다.
