41f9588576663fd566832019f357f632bd48d097
JinsuNa <shs1401009@gmail.com>
2025-01-15 10:38:09 +0900

    Update a와 b 출력하기.java

--- Programmers/Lv0/a와 b 출력하기.java
+++ Programmers/Lv0/a와 b 출력하기.java
@@ -10,3 +10,17 @@ public class Solution {
         System.out.println("b = "+ b);
     }
 }
+
+// 줄바꿈으로 하는 다른 방법의 풀이 두줄이었던 sout이 한줄로 바뀐다.
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+a +"\n" +"b = "+b);
+    }
+}
