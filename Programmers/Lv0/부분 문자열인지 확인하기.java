6fe7dc4674b80ea6fe01fb096e56bd1f32d732e3
JinsuNa <shs1401009@gmail.com>
2025-01-16 23:45:59 +0900

    Create 부분 문자열인지 확인하기.java

---
+++ Programmers/Lv0/부분 문자열인지 확인하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        if(my_string.contains(target)){
+            return 1;
+        }
+        return answer;
+    }
+}
+
+//이것도 문자열 확인과 똑같은 방식이라 중복이다.
