29a7b454012bc0359dac88d6b69f8ad0a58a5297
JinsuNa <shs1401009@gmail.com>
2025-01-16 18:56:24 +0900

    Create 문자 리스트를 문자열로 변환하기.java

---
+++ Programmers/Lv0/문자 리스트를 문자열로 변환하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        for(String s : arr){
+        answer += s;    
+        }
+        
+        return answer;
+    }
+}
+
+// for 문에 넣고 answer 을 ++하면 끝
