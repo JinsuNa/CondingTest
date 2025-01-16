08f65a2cc7c1798a0e9a86663b78838e23655cff
JinsuNa <shs1401009@gmail.com>
2025-01-16 09:47:41 +0900

    Update 문자열 겹쳐쓰기.java

--- Programmers/Lv0/문자열 겹쳐쓰기.java
+++ Programmers/Lv0/문자열 겹쳐쓰기.java
@@ -18,3 +18,13 @@ class Solution {
     }
 }
 
+
+// 다른방법
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        return before + overwrite_string + after;
+    }
+}
